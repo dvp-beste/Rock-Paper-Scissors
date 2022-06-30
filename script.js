72773d2147105452cf77669aff9f4e5e246e8f93
Beste <besteeris@gmail.com>
2022-06-30 11:08:30 +0300

    Add UI with buttons and addeventlistener
    Add buttons and results to the index file
    Add addeventlisteners to the script so that one can play so many rounds until either the player or the computer reaches 5 points. Add a new game button so that the player can start a new game at any given point.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const roundResult = document.querySelector('#RoundResult');
+const playerScore = document.querySelector('#PlayerScore');
+const computerScore = document.querySelector('#ComputerScore');
+const gameResult = document.querySelector('#GameResult');
+const newGameButton = document.querySelector('#NewGame');
+
+
+
+let playerPoints = 0;
+let computerPoints = 0;
+playerScore.textContent = playerPoints;
+computerScore.textContent = computerPoints;
+
+newGameButton.addEventListener('click', () => {
+    playerPoints = 0;
+    computerPoints = 0;
+    playerScore.textContent = playerPoints;
+    computerScore.textContent = computerPoints;
+    gameResult.textContent = '';
+})
+
 function computerPlay() {
     let randomInteger = Math.floor(Math.random()*3);
     if (randomInteger === 0) {
@@ -11,10 +32,15 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    const isDraw = playerSelection === computerSelection;
+
+    if (isDraw) {
+        return "Draw";
+    }    
+
     if (playerSelection === "Rock") {
-        if (computerSelection === "Rock") {
-            return "Draw!";
-        } else if (computerSelection === "Paper") {
+        if (computerSelection === "Paper") {
             return "You Lose! Paper beats Rock";
         } else {
             return "You Win! Rock beats Scissors";
@@ -23,8 +49,6 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
             return "You Win! Paper beats Rock";
-        } else if (computerSelection === "Paper") {
-            return "Draw!";
         } else {
             return "You Lose! Scissors beats Paper";
         }
@@ -32,18 +56,54 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Scissors") {
         if (computerSelection === "Rock") {
             return "You Lose! Rock beats Scissors";
-        } else if (computerSelection === "Paper") {
-            return "You Win! Scissors beats Paper";
         } else {
-            return "Draw!";
+            return "You Win! Scissors beats Paper";
         }
     }
 }
 
-function game() {
+
+const buttons = document.querySelectorAll('.selection');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        roundResult.textContent = playRound(button.id, computerPlay());
+
+        if (roundResult.textContent.includes("Win")) {
+            playerPoints++;
+            playerScore.textContent = playerPoints;
+        } else if (roundResult.textContent.includes("Lose")) {
+            computerPoints++;
+            computerScore.textContent = computerPoints;
+        }
+        if (playerPoints === 5 || computerPoints === 5) {
+             if (playerPoints === 5) {
+                gameResult.textContent = `YOU WON THE GAME! YOUR SCORE: ${playerPoints} - COMPUTER'S SCORE: ${computerPoints}`;
+            } else if (computerPoints === 5) {
+                gameResult.textContent = `YOU LOST THE GAME! YOUR SCORE: ${playerPoints} - COMPUTER'S SCORE: ${computerPoints}`;
+            }        
+            playerPoints = 0;
+            computerPoints = 0;
+            playerScore.textContent = playerPoints;
+            computerScore.textContent = computerPoints;
+        }
+       
+
+    });
+});
+
+
+
+
+// button.addEventListener('click, playRound(button.id, computerPlay())
+/* Passing Parameters
+When passing parameter values, use an "anonymous function" that calls the specified function with the parameters:
+*/
+
+/* function game() {
     let playerPoints = 0;
     let computerPoints = 0;
-    for (let i = 1; i < 6; i++) {
+    for (let i = 1; i < 2; i++) {
         let roundResult = playRound(prompt(`Round ${i}. Choose Rock, Paper or Scissors`), computerPlay());
         if (roundResult.includes("Win")) {
             playerPoints++;
@@ -54,7 +114,7 @@ function game() {
         } else {
             console.log(`${roundResult}. You have ${playerPoints} points at round ${i}!`);
         }
-    } 
+    }
     if (playerPoints > computerPoints) {
         return "You won the game!";
     } else if (playerPoints < computerPoints) {
@@ -62,4 +122,6 @@ function game() {
     } else {
         return "Draw!"
     }
-}
+} */
+
+//console.log(game());
