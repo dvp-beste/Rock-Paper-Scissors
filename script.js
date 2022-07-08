938516a66c9b71c4437ae56e40c78bf65c149404
Beste <besteeris@gmail.com>
2022-07-08 14:24:17 +0300

    Remove unnecessary 'else'

--- script.js
+++ script.js
@@ -27,14 +27,13 @@ function startNewGame() {
 newGameButton.addEventListener('click', startNewGame);
 
 function computerPlay() {
-    let randomInteger = Math.floor(Math.random()*3);
+    const randomInteger = Math.floor(Math.random()*3);
     if (randomInteger === 0) {
         return "Rock";
     } else if (randomInteger === 1) {
         return "Paper";
-    } else {
-        return "Scissors";
     }
+    return "Scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -52,32 +51,31 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
             return "You Lose! Paper beats Rock";
-        } else {
-            return "You Win! Rock beats Scissors";
         }
+        return "You Win! Rock beats Scissors";
     }
+
     if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
             return "You Win! Paper beats Rock";
-        } else {
-            return "You Lose! Scissors beats Paper";
         }
+        return "You Lose! Scissors beats Paper";
     }
+
     if (playerSelection === "Scissors") {
         if (computerSelection === "Rock") {
             return "You Lose! Rock beats Scissors";
-        } else {
-            return "You Win! Scissors beats Paper";
         }
+        return "You Win! Scissors beats Paper";
     }
 }
 
-
 const buttons = document.querySelectorAll('.selection');
+const winScore = 5;
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        if (playerPoints === 5 || computerPoints === 5) {
+        if (playerPoints === winScore || computerPoints === winScore) {
             startNewGame();
         }
         
@@ -90,54 +88,14 @@ buttons.forEach((button) => {
             computerPoints++;
             computerScore.textContent = computerPoints;
         }
-        if (playerPoints === 5 || computerPoints === 5) {
-             if (playerPoints === 5) {
+        if (playerPoints === winScore || computerPoints === winScore) {
+            if (playerPoints === winScore) {
                 gameResult.setAttribute('style', 'white-space: pre;');
                 gameResult.textContent = `YOU WON THE GAME! \r\n YOUR SCORE: ${playerPoints} - COMPUTER'S SCORE: ${computerPoints}`;
-            } else if (computerPoints === 5) {
+            } else if (computerPoints === winScore) {
                 gameResult.setAttribute('style', 'white-space: pre;');
                 gameResult.textContent = `YOU LOST THE GAME! \r\n YOUR SCORE: ${playerPoints} - COMPUTER'S SCORE: ${computerPoints}`;
             }        
-  /*           playerPoints = 0;
-            computerPoints = 0;
-            playerScore.textContent = playerPoints;
-            computerScore.textContent = computerPoints; */
         }
-       
-
     });
 });
-
-
-
-
-// button.addEventListener('click, playRound(button.id, computerPlay())
-/* Passing Parameters
-When passing parameter values, use an "anonymous function" that calls the specified function with the parameters:
-*/
-
-/* function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for (let i = 1; i < 2; i++) {
-        let roundResult = playRound(prompt(`Round ${i}. Choose Rock, Paper or Scissors`), computerPlay());
-        if (roundResult.includes("Win")) {
-            playerPoints++;
-            console.log(`${roundResult}. You have ${playerPoints} points at round ${i}!`);
-        } else if (roundResult.includes("Lose")) {
-            computerPoints++;
-            console.log(`${roundResult}. You have ${playerPoints} points at round ${i}!`);
-        } else {
-            console.log(`${roundResult}. You have ${playerPoints} points at round ${i}!`);
-        }
-    }
-    if (playerPoints > computerPoints) {
-        return "You won the game!";
-    } else if (playerPoints < computerPoints) {
-        return "You lost the game!";
-    } else {
-        return "Draw!"
-    }
-} */
-
-//console.log(game());
